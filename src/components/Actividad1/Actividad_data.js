7ab3001e657c818ecb37c16807a48e7d869bc4a2
ccss <ccss@utp.edu.co>
2020-03-10 15:47:46 -0500

    finished logic for this module, still left styles

---
+++ src/components/Actividad1/Actividad_data.js
@@ -0,0 +1,58 @@
+const data = [
+
+    {
+      "right": 0,
+      "answer": 'false',
+      "text": "There are videos with guided meditations in Headspace."
+    },
+    {
+      "right": 0,
+      "answer": 'na',
+      "text": "One of the apps organizes your exercise routines."
+    },
+    {
+      "right": 0,
+      "answer": 'true',
+      "text": "There is one app dedicated to money management."
+    },
+    {
+      "right": 0,
+      "answer": 'true',
+      "text": "There is one app that users of social network can use."
+    },
+    {
+      "right": 0,
+      "answer": 'false',
+      "text": "You can use one app to play different video files."
+    },
+    {
+      "right": 0,
+      "answer": 'false',
+      "text": "There are videos with guided meditations in Headspace."
+    },
+    {
+      "right": 0,
+      "answer": 'true',
+      "text": "There is one app that is not available in Colombia."
+    },
+    {
+      "right": 0,
+      "answer": 'false',
+      "text": "All the apps are available in app store (Iphone) and the play store (Android)."
+    },
+    {
+      "right": 0,
+      "answer": 'na',
+      "text": "There is one app that helps you concentrate when you study."
+    },
+    {
+      "right": 0,
+      "answer": 'false',
+      "text": "Any.do takes little memory space in your cellphone."
+    }
+
+
+  ]
+
+
+export default data
